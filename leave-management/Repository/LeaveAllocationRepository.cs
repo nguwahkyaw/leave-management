cb35372e8b6960d8705fac0b6dacdc30e4b4dd1a
NguWah Kyaw <nguwahkyaw5@gmail.com>
2020-11-22 00:04:20 +0630

    Added LeaveType Repositories and CRUD and Added Database and Font Awesome Icon

--- leave-management/Repository/LeaveAllocationRepository.cs
+++ leave-management/Repository/LeaveAllocationRepository.cs
@@ -39,6 +39,12 @@ namespace leave_management.Repository
             return leaveAllocation;
         }
 
+        public bool isExitsts(int id)
+        {
+            var exists = _db.LeaveAllocations.Any(q => q.Id == id);
+            return exists;
+        }
+
         public bool Save()
         {
             var changes = _db.SaveChanges();
